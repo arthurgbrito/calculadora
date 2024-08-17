56f181c6410ba39817f14d5ffb93d486f83364f5
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2024-08-17 11:57:15 -0300

    Fazendo alterações no script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let visor = document.querySelector('.visor');
-let exp
+let exp = ''
 
 function adicionaValor(valor){
   let ultimo = visor.value.charAt(visor.value.length - 1)
@@ -7,25 +7,74 @@ function adicionaValor(valor){
     if ( ultimo == ''){
       alert('Não é possível usar essa opção! Digite algo válido!')
     } else {
-      visor.value += valor;
+      visor.value += valor
+      exp += valor  
     }
   } else {
-    visor.value += valor;
+    visor.value += valor
+    exp += valor 
   }
 }
 
 function apagaVisor(){
   visor.value = ''
+  exp = ''
 }
 
 function del(){
   visor.value = visor.value.slice(0, -1)
+  exp = exp.slice(0, -1)
 }
 
 function expressao() {
   try {
-      visor.value = eval(visor.value);
+
+    exp = exp.replace(/x/g, '*').replace(/÷/g, '/')
+    
+    let resultado = eval(exp)
+
+    if (!isNaN(resultado) && resultado % 1 !== 0){
+      resultado = parseFloat(resultado.toFixed(11))
+    }
+
+    visor.value = resultado
+      
   } catch {
       visor.value = 'ERRO';
   }
 }
+
+document.addEventListener('keydown', function(evento) {
+
+  let key = evento.key
+
+  let botao
+
+  if (!isNaN(key) && key !== ' '){
+
+    botao = document.querySelector(`input[value="${key}"]`)
+  
+  }
+  
+  switch(key){
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+      botao = document.querySelector(`input[value="${key}"]`)
+      break;
+    case 'Enter':
+      botao = document.querySelector('input.igual')
+      break;
+    case 'Backspace': 
+      botao = document.querySelector('input.backspace')
+      break;
+    case '.': 
+      botao = document.querySelector('input.virgula')
+    break;
+  }
+
+  if (botao){
+    botao.click()
+  }
+})
