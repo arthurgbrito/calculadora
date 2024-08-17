476f37ad5dab53b9db662e48b02b2f1e635bfeed
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2024-08-16 21:01:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let visor = document.querySelector('.visor');
+let exp
 
 function adicionaValor(valor){
-  visor.value += valor;
+  let ultimo = visor.value.charAt(visor.value.length - 1)
+  if (valor == 'x' || valor == '-' || valor == '+' || valor == '÷'){
+    if ( ultimo == ''){
+      alert('Não é possível usar essa opção! Digite algo válido!')
+    } else {
+      visor.value += valor;
+    }
+  } else {
+    visor.value += valor;
+  }
 }
 
 function apagaVisor(){
   visor.value = ''
+}
+
+function del(){
+  visor.value = visor.value.slice(0, -1)
+}
+
+function expressao() {
+  try {
+      visor.value = eval(visor.value);
+  } catch {
+      visor.value = 'ERRO';
+  }
 }
